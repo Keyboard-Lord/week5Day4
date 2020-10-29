2abffa0f300b95df7207c2eadc131ce833f7c37a
Keyboard-Lord <tvmcrae@gmail.com>
2020-10-28 20:05:39 -0600

    commitment

---
+++ app.js
@@ -0,0 +1,7 @@
+
+let tyler = 'Tyler'
+$('a').css('color','white');
+$('#nav').css('background-color','#145')
+$('li:even').css('background-color','#ccc')
+$('li:odd').css('background-color','#aaa')
+$('input').val(tyler)
